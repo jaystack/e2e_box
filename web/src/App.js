9ffbe52266b973343109c760689ab4d2fdfac647
PeterAronZentai <peter.zentai@avangrade.com>
2017-07-06 08:59:51 +0100

    add site functionality

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 
 
-const simulateNetworkCall = url => ({})
+const Item = ({x, y}) => <div className="item">{x}+{y}={(x + y)}</div>
 
 class App extends Component {
   constructor() {
@@ -14,16 +14,38 @@ class App extends Component {
     }
   }
 
+  addItems(e) {
+    e.preventDefault();
+    const { x: { valueAsNumber: x }, y : { valueAsNumber: y }, form } = this.refs;
+    this.setState({loading: true})
+    setTimeout(() => {
+      this.setState({
+        items: [...this.state.items, {x, y}],
+        loading: false,
+      })
+      form.reset();
+    }, Math.random() * 2000)
+  }
+
   render() {
+    const { loading, items }  = this.state;
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" data-welcome>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Add X to Y and see results
         </p>
+        <form onSubmit={ (e) => this.addItems(e) } id="theform" ref="form">
+          X: <input name="X" ref="x" type="number" data-x required placeholder="number"/>
+          Y: <input name="Y" ref="y" type="number" data-y required placeholder="number"/>
+          <button data-submit disabled={loading}>Add{loading && 'ing'}</button>
+        </form>
+        {!!items.length && <div data-results>
+          {items.map( ({x, y}, i) => <Item  key={i} x={x} y={y} />)}
+        </div>}
       </div>
     );
   }
